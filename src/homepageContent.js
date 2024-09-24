95b667b2750b4d1de290c5a88476fd45e07e9135
Iversson.Estudos <iversson.estudos@gmail.com>
2024-09-24 15:04:22 -0300

    function to create homepage

---
+++ src/homepageContent.js
@@ -0,0 +1,24 @@
+const image = document.createElement('img');
+const headline = document.createElement('h1');
+const underheadText = document.createElement('p');
+
+image.src = "https://placehold.co/600x400/000000/FFF?text=PLACEHOLDER";
+
+headline.textContent="Welcome to the Belly";
+
+underheadText.innerHTML='If you want to prove the best dishes of our city, You came to the right place,<br>';
+underheadText.innerHTML+='Dishes prepared by the most requinted cheffs of the world<br>';
+underheadText.innerHTML+='Handpicked ingredients, produced by locals, following all the michelin standards on their growth<br>';
+underheadText.innerHTML+='Drinks from all over the world, so you thirst has no chance of ruining your appetite<br>';
+
+
+
+function homepage(){
+    const content =  document.getElementById('content');
+    content.appendChild(image);
+    content.appendChild(headline);
+    content.appendChild(underheadText);
+}
+
+
+export {homepage};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./styles.css";
+import {homepage} from "./homepageContent.js";
+
+homepage();
 
