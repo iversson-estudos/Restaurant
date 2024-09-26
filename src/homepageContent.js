ff52347a30db1442197b7b6831d6f1a3901c56d4
Iversson.Estudos <iversson.estudos@gmail.com>
2024-09-26 20:29:13 -0300

    continuing styling homepage

--- src/homepageContent.js
+++ src/homepageContent.js
@@ -3,26 +3,25 @@ import logo from "./img/logo.png"
 
 
 const image = document.createElement('img');
-const headline = document.createElement('h1');
 const underheadText = document.createElement('p');
 
 image.src = logo;
+image.id = 'homepageImg'
 
-headline.textContent="DELICIAS DO SUL";
+
 
 underheadText.innerHTML='Sejam bem vindos a melhor padaria de Porto Nacional<br>';
 underheadText.innerHTML+='Aqui você encontra salgados e doces com muita variedade e sabor<br>'
 underheadText.innerHTML+='Todos feitos com muito carinho e amor<br>';
-const headerDiv = document.createElement('div');
-headerDiv.id='headerDiv';
-headerDiv.appendChild(headline);
-headerDiv.appendChild(image);
+const infoDiv = document.createElement('div');
+infoDiv.id='infoDiv';
+infoDiv.appendChild(image);
 
 function homepage(){
     
     const content =  document.getElementById('content');
     content.innerHTML='';
-    content.appendChild(headerDiv);
+    content.appendChild(infoDiv);
     content.appendChild(underheadText);
 }
 
