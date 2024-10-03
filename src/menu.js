e9cf912fed06a2b6d0d0cc1e9b633aa4ded763c3
Iversson.Estudos <iversson.estudos@gmail.com>
2024-10-03 12:16:29 -0300

    finished all tabs, now to fine detailing

--- src/menu.js
+++ src/menu.js
@@ -1,44 +1,37 @@
-import bolococo from "./img/bolococo.png"
-import cafe from "./img/cafe.png"
-import cappucinno from "./img/cappucinno.png"
-import enroladinhodequeijo from "./img/enroladinhodequeijo.png"
-import roscas from "./img/roscas.png"
-import sanduichenatural from "./img/sanduichenatural.png"
+import bolococo from "./img/bolococo.png";
+import cafe from "./img/cafe.png";
+import cappucinno from "./img/cappucinno.png";
+import enroladinhodequeijo from "./img/enroladinhodequeijo.png";
+import roscas from "./img/roscas.png";
+import sanduichenatural from "./img/sanduichenatural.png";
 
-
-
-const  produtos = [
-    {nome:"Bolo de Coco", imagem: bolococo},
-    {nome:"Café", imagem: cafe},
-    {nome:"Cappucinno", imagem: cappucinno},
-    {nome:"Enroladinho de Queijo", imagem: enroladinhodequeijo},
-    {nome:"Roscas", imagem: roscas},
-    {nome:"Sanduiche Natural", imagem: sanduichenatural}
+const produtos = [
+  { nome: "Bolo de Coco", imagem: bolococo },
+  { nome: "Café", imagem: cafe },
+  { nome: "Cappucinno", imagem: cappucinno },
+  { nome: "Enroladinho de Queijo", imagem: enroladinhodequeijo },
+  { nome: "Roscas", imagem: roscas },
+  { nome: "Sanduiche Natural", imagem: sanduichenatural },
 ];
 
+function menu() {
+  const content = document.getElementById("content");
+  content.innerHTML = "";
 
+  produtos.forEach((item) => {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
 
-function menu(){
-    
-    const content =  document.getElementById('content');
-    content.innerHTML='';
+    p.textContent = item.nome;
+    img.src = item.imagem;
+    img.id = "produto";
+    div.id = "menu";
+    div.appendChild(p);
+    div.appendChild(img);
 
-    produtos.forEach((item)=>{
-        const div = document.createElement('div');
-        const p = document.createElement('p');
-        const img = document.createElement('img');
-
-        p.textContent = item.nome;
-        img.src = item.imagem;
-        img.id = "produto";
-        div.appendChild(p);
-        div.appendChild(img);
-        
-        
-       content.appendChild(div);
-
-    });
-
+    content.appendChild(div);
+  });
 }
 
-export {menu};
+export { menu };
--- src/sobre.js
+++ src/sobre.js
@@ -1,25 +1,24 @@
-import ambiente from "./img/ambiente.png"
+import suelen from "./img/suelen.png";
+const img = document.createElement("img");
+const historia = document.createElement("p");
 
-const img = document.createElement('img');
-const historia = document.createElement('p');
+img.src = suelen;
 
-img.src = ambiente;
+historia.innerHTML =
+  "Olá meu nome é Suelen Kaufert Zanella e eu sou a proprietária da padaria Delicias do Sul<br>";
+historia.innerHTML +=
+  "Sou formada em panificação e confeitaria e atuo na área à mais de 10 anos<br>";
+historia.innerHTML +=
+  "Sempre buscando inovar e aperfeiçoar minha técnica e buscar novidades para meus clientes<br>";
 
-
-historia.innerHTML = 'Olá meu nome é Suelen Kaufert Zanella e eu sou a proprietária da padaria Delicias do Sul<br>'
-historia.innerHTML +='Sou formada em panificação e confeitaria e atuo na área à mais de 10 anos<br>';
-historia.innerHTML +='Sempre buscando inovar e aperfeiçoar minha técnica e buscar novidades para meus clientes<br>';
-
-
-function sobre (){
-
-const div = document.createElement('div');
-const content =  document.getElementById('content');
-div.appendChild(img);
-div.appendChild(historia);
-content.innerHTML='';
-content.appendChild(div);
-
+function sobre() {
+  const div = document.createElement("div");
+  const content = document.getElementById("content");
+  div.id = "sobre";
+  div.appendChild(img);
+  div.appendChild(historia);
+  content.innerHTML = "";
+  content.appendChild(div);
 }
 
-export {sobre};
+export { sobre };
